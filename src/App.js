42967ec87a8ed6a818b1e017c9dc456d9be4b1e2
sagar <rebelsagarx@gmail.com>
2022-02-25 18:01:40 +0530

    internal component use

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 import User from './User';
 
 function App() {
+  function Apple(){
+    return(<div>hello internal function component</div>)
+  }
   return (
     <div className="App">
      <h1>Hello </h1>
+     <Apple/>
+     {/* {Apple()} */}
      < User/>
      < User/>
     
