812678ccf1797962d448de83b7a6a8a964ae2f38
sagar <rebelsagarx@gmail.com>
2022-02-25 19:20:08 +0530

    State Vs variables

--- src/App.js
+++ src/App.js
@@ -1,31 +1,38 @@
+import React,{useState} from "react"
 import './App.css';
 
-// alert("helo button")
 
 function App() {
-  let data = "sagar"
-  function apple(){
-data="peter"
-    alert(data)
-    //we want to change h1 data to peter when button clicked
-    // it changes the data in alert but cant changes data on another function when button clicked thats why we use state and props
-  //use less variables when change state of something
-  }
-  
+  // let data = "sagar"
+  // function updateData(){
+  //      data="peter"
+  //      alert(data)
+  // }
+  // console.log("_____________________");\
+
+const [data, setData]=useState(4)
+console.log(data);
+function updateData(){
+  setData(data/2);
+}
+console.log("this is re render again when button clicked and state changes again__________________________________________________");
+// watch this log on console = it will update or re render again when we click on button
   return (
     <div className="App">
-     <h1>Event!{data} </h1>
+     <h1>State {data}</h1>
+     <button onClick={updateData}>Update Data</button>
 
-  {/* <button onClick={apple}>Click Me</button> */}
-  {/* if we want to call directly a fuction then we use arrow fun or may be call back fun */}
-  {/* with the help of arrow fun the function cant call itself ,it call when clicking on button */}
-  <button onClick={()=>apple()}>Click Me</button>
 
-  {/* <button onClick={()=>alert("alert")}>Click Me</button> */}
+{/* useState IS a hook and it allows us to maintain state , update state= we can use this hook by destructure and define by variables*/}
+    {/* <div className="App">
+     <h1>State {data}</h1>
+     <button onClick={updateData}>Update Data</button> */}
+     {/* in state , component will re render again , or update , and it is for state only not for variables,and variable cant changes the state because it cant re rendered again , it stay in freeze */}
+     {/* react not working this style , to  change data = props and state in react = it does not understand variable , it only understand state and props*/}
+     {/* state is an object = in variable we can store data ..same as in state we can store data and print on when called
+     state only use in react */}
 
   
-    
-
     </div>
   );
 }
