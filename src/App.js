1c5c96b4ea331f06948318585d103a216b441a98
sagar <rebelsagarx@gmail.com>
2022-02-26 21:42:16 +0530

    Constructor Life Cycle Methods

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import './App.css';
-import React from "react"
-import User from "./User"
-import Member from './Member';
+import React, { Component } from "react"
+// import Member from './Member';
 
-function App(){
+class App extends Component{
 
-  function getData(){
-    alert("hello from app.js")
+  constructor(){
+    super()
+    //why we call super in constructor? jo eska parent constructor hot hai jo component class hai ..usko call krne ke liye hum super ko call krte hai
+   //ye constructor sabse phele ban jata ha tho hum state yji pe define krte hai,is se kya hoga state humari phele he ban jayegi html ready hone se phele,or easily hum print krwa skte hai with the help of state
+   //esa nhi ho ki state humari ready hui nhi hai , or uss se phele humara html ready ho gya, toh problem aayegi tbhi hum constructor use krte hai state ko define krne ke liye
+   //or kya nhi kr skte hai?yha pe kbhi apko api call nhi krni chiyea?kyunki humara constructor func and html ready nhi hota ,or uss se phele api ready ho jatti hai toh problem aa jati hai ...uske liye hum react mai component did mount method usse krte hai .api's ko call krne ke liye
+   console.log("my name is constructor");
+    this.state={
+      data:"sagar"
+    }
   }
-   
 
-      return (
-        <div className="App">
-         {/* why we pass function from parent component to child component?why we cant create function in child component? 
-         because if we need or reuse that function on another child component so that we can use it from parent component
-         creating another child component member.js to reuse that function which we are using in user.js*/}
-         {/* can we call this function in super child component?if yes  then how ?if not why? */}
-          <User data={getData}/>
+   render(){
+console.log("i am render");
+    return (
+      <div className="App">
+        <h1>Constructor Life cycle methods  {this.state.data}</h1>
+        <p>Constructor call first before any component</p>
 
-          <div style={{ color:"salmon"}}>
-          <Member data={getData}/>
 
-          </div>
 
-  
-        </div>
-      )
+
+      </div>
+    )
+
+   }
+
+    
  
 }
 
--- src/Member.js
+++ src/Member.js
@@ -7,8 +7,14 @@ function Member(props){
 
     return(
                 <div>
-                <h1>Reusing function in Member.js</h1>
-                <button onClick={props.data}>call data function</button>
+                <h1>Life cycle methods</h1>
+                <h2>
+                    three phases of life cycle
+                    1.birth=mount  ===product load  ==componentDidMOUNT 
+                    2.growth=update  ===product update  ==COMPONENTdidmount
+                    3.death=unmount    ==product remove ==componentwillmount
+                </h2>
+                <button>call data function</button>
 
                 </div>
         
