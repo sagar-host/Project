25b7d64707b85a6473831a40bb40394fb8bb73d4
sagar <rebelsagarx@gmail.com>
2022-02-26 20:41:35 +0530

    Condition in React

--- src/App.js
+++ src/App.js
@@ -1,38 +1,15 @@
 import './App.css';
 import React,{useState} from "react"
+import Profile from "./Profile"
 
 function App(){
 
-const [name , setName] = useState("")
-const [policy, setPolicy]=useState(false)
-const [intrest , setIntrest] = useState("")
-
-
-function getFormData(e){
-  console.log(name,policy,intrest);
-e.preventDefault()
-}
    
 
       return (
         <div className="App">
-         <h1>Handle Forms in React</h1> 
-{/* to prevent not to refresh page on form submit , we should use event on form onsubmit() 
-and make function and it take parameter , and write e.preventdefault - actually it is a functin , it stop the form to submit*/}
-        
-         <form onSubmit={getFormData}>
-           <input type="text" placeholder='enter name' onChange={(e)=>setName(e.target.value)}/><br /> <br />
-           <select onChange={(e)=>setIntrest(e.target.value)}>
-           <option>select option</option>
-             <option>Marvel</option>
-             <option>Dc comics</option>
-           </select><br /> <br />
-           <input type="checkbox" onChange={(e)=>setPolicy(e.target.checked)} /> <span>Accept privacy policy</span><br /> <br />
-           {/* checked takes true and false */}
-           <button type='submit'>Submit</button>
-           {/* we can also use value to control the form */}
-           {/* how we can disable a particular field */}
-         </form>
+         <h1>Conditional Rendering | if condition</h1> 
+          <Profile/>
   
         </div>
       )
