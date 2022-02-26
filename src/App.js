a311d587d05af24a37cc37b4e763f14a02f5acdb
sagar <rebelsagarx@gmail.com>
2022-02-27 01:50:11 +0530

    hook - useState

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
 import './App.css';
-import React, { Component, useState } from "react"
-import Student  from './Student';
+import React, { useState } from "react"
 
 
-class App extends Component{
+function App (){
+ 
+  const [data, setData] = useState("sagar")
 
-  constructor(){
-    super();
-    console.log("constructor");
-    this.state={
-     show: true
-    }
-  }
-
-
-render(){
-  console.log("render");
   return (
     <div className="App">
-    {
-      this.state.show ?  <Student/>: <h1>child component removed</h1>
-    }
-   {/* not operator= use for toggle */}
-   {/* jaise he child component remove hoga toh ComponentWillUnmount automatic call hoga */}
-    {/* <h1>ComponentWillUnmount, life cycle method   </h1> */}
-      {/* <p>when component will remove from then ComponentWillUnmount method will call, when we hide/show ,hide something then that will completely remove from dom</p> */}
-     
-      <button onClick={()=>{this.setState({show:!this.state.show}) 
-      console.log("deleting")}}>Toogle child component</button>
+         <h1>Hooks {data}</h1>
+         {/* <p>why hooks? because hooks are used in functional component,but in functional component we use hooks to apply life cycle methods</p>
+           <p>when we are using class component , inside class component they have inbuilt features like state,lifecycle methods,pure component,etc </p>
+           <p>but in functional component there are not any bult in features , so with hook, we can use class component features in functional components such as state,life cycle,pure component,etc</p>
+   <p>useState is a hook, just type use and vscode recommend hooks</p>
+    
+   <p>usestate can be use in destructuring method of javascript</p> */}
+   <p>we use use effect hook because it contain all life cycle methods</p>
+   <button onClick={()=>setData("xxxx")}>data</button>
     </div>
   )
 
-}
 
- 
-    
- 
+
 }
 
 export default App;
