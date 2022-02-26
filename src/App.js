a3e6de5e567520ce74d8879d24fa5deff8a8af66
sagar <rebelsagarx@gmail.com>
2022-02-26 23:20:25 +0530

    ComponentDidMount Method

--- src/App.js
+++ src/App.js
@@ -1,30 +1,40 @@
 import './App.css';
-import React, { useState } from "react"
-import User from "./User"
+import React, { Component, useState } from "react"
+// import User from "./User"
 
 
-function App(){
+class App extends Component{
 
-// const [name, setName] = useState("sagar")
+  constructor(){
+    super();
+    // console.log("constructor");
+    this.state={
+      name:"sagar"
+    }
+  }
 
+componentDidMount(){
+console.log("mount");
+}
 
-    return (
-      <div className="App">
-        {/* <h5>Render(execute krke represent krna)method in React = basically it is also a life cycle method</h5>
-        <p> when state or prop update then render method itself run or automatically run 
-        when render method calls?1.when component will ready for execution, 2.when props update and 3. when state channges
-        </p>  for using state comment out button h1 and use contructor in user.js*/}
-       {/* <h1><User name={name} /></h1> */}
-       {/* <button onClick={()=>setName("xxx")}>update name</button> */}
-     
-       <h1><User/></h1>
-      </div>
-    )
+render(){
+  console.log("render");
+  return (
+    <div className="App">
+    <h1>{this.state.name}</h1>
+     {/* <h1>componentDidMount, life cycle method</h1>
+     <p>mount means when html css and dom all thing is ready then this fun call, it uses when we call api's,operations or any other method that depend on html </p>
+    <p>no effect on state and props</p> */}
+    <p>hum api call only call karenge jab page load hoga , bar bar api thoda na call karenge tabhi hum did mount use krte hai jo ki sirf ek he bar call hota hai jb page load hota hai only , tabhi props , state ka koi effect nhi pdta iske upar</p>
+    <button onClick={()=>this.setState({name: "cccccc"})}>update name</button>
+    </div>
+  )
 
+}
+
+ 
     
  
 }
-
-
 
 export default App;
