ba428c87c427e883da2095f2a89d0fc9899d8131
sagar <rebelsagarx@gmail.com>
2022-02-27 19:36:33 +0530

    List with Bootstrap and Uniue Key

--- src/App.js
+++ src/App.js
@@ -1,60 +1,48 @@
 import "./App.css";
 import React from "react";
+import {Table} from "react-bootstrap"
 
 
 
 function App() {
 
-  // const students = ["sagar","anil","peter","sam"];
-
-
-  //we use objects for seprate fields
-const students = [
-{name:"sagar" , email:"rebelsagarx@mail.com", contact:988222666621},
-{name:"peter" , email:"peter@mail.com", contact:66522588416224},
-{name:"pinky" , email:"pinky@mail.com", contact:78844661122},
-{name:"zukerberg" , email:"zukerberg@mail.com", contact:786}
+const users = [
+{name:"sagar" , email:"rebelsagarx@mail.com", contact:111},
+{name:"peter" , email:"peter@mail.com", contact:786},
+{name:"pinky" , email:"pinky@mail.com", contact:111},
+{name:"zukerberg" , email:"zukerberg@mail.com", contact:111}
 
 
 ]
 
 
-  //map is use for looping ...traverse elements in array
 
-  // students.map((item)=>{
-  //   console.log("My name is: ",item);
-  // })
-
-  // for(let i=0; i<students.length;i++){
-  //     console.log("in for loop so name is", students[i]);
-  // }
-
-  //why we use map instead of for loop because inside return the for loop does not support at all but map function support it.
 
   return (
     <div className="App">
-        
-        <h1>Handle Array with list</h1>
-        <p>map function vs for loop</p>
-        map function
-       
-       <table border="2">
-       <tr>
-            <td>Name</td>
-            <td>Email</td>
-            <td>Contact</td>
-          </tr>
+    {/* //unique key prop  actually need a unique key to identify a particular item otherwise its virtual dom not work properly*/}
+      {/* how to show conditionally in table?only show contact who 111 only  = iss tarah filter apply kr skte hai */}
+        <h1>List with Bootstrap and Uniue Key</h1>
+        <Table striped bordered hover size="sm" variant="dark">
+        <tbody>
+        <tr>
+                <td>Name</td>
+                <td>Email</td>
+                <td>Contact</td>
+              </tr>
 
-       {students.map((data)=>
-          <tr>
-            <td>{data.name}</td>
-            <td>{data.email}</td>
-            <td>{data.contact}</td>
-          </tr>
-      
-       )}
-       </table>
 
+          {
+            users.map((item, i)=>
+              item.contact === 111 ? <tr key={i}>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>{item.contact}</td>
+              </tr> :null
+           )
+          }
+          </tbody>
+          </Table>
 
     </div>
   );
