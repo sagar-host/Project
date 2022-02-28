3e29480fca30988340e95a5a15e4b6bf7f5bd25a
sagar <rebelsagarx@gmail.com>
2022-02-28 15:02:07 +0530

    forward Ref

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import "./App.css";
 import React,{useRef} from "react";
+import User from "./User"
 
 function App  (){
      
   let inputRef = useRef(null)
 
-  function handleInput(){
-console.log("calling");
-// inputRef.current.value = "1000"
-// inputRef.current.focus()
-// inputRef.current.style.color ="gold"
-inputRef.current.style.display= "none"
+  function updateInput(){
+inputRef.current.value ="1000"
+inputRef.current.style.color ="red"
+inputRef.current.focus()
+
+
   }
       return (
         <div className="App">
-           <h1>Ref in functional component- it is a hook</h1>
-           <p>it is only use for emergency where props and state not work</p>
-            <input type="text" ref={inputRef} />
-            <button onClick={handleInput}>ref</button>
-          
-       
+           <h1>forward Ref</h1>
+           {/* <p>if input is in child component and button is in parent component, and we want to change its dom , when button is clicked from parent component, so we use forward ref hook</p> */}
+            <User ref={inputRef}/>
+             <button onClick={updateInput}>update input box</button>
+
            </div>
           
          );
