429fbf7ffd4072397f3b10389489fa6d3c349b81
sagar <rebelsagarx@gmail.com>
2022-02-28 16:10:12 +0530

    UnControlled components in Reactjs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
 import "./App.css";
-import React,{useState} from "react";
+import React, { useRef} from "react";
 
-function App  (){
-     let [val, setVal] = useState("000")
-     let [item, setItem] = useState("")
+function App() {
+  let inputRef = useRef(null);
+  let inputRef2 = useRef(null);
 
-      return (
-        <div className="App">
-           <h1>Controlled components in Reactjs</h1>
-                  {/* <p>it is related to input field or in form there are 3-4 input fields </p>
-                  <p>react ke andar jab hum input fields ko state ke through control krte hai to unko khete hai Controlled component</p>
-                  <p>uncontrolled component wo hote hai jo ki directly dom ke through handle hote hai jaise ki jQuery or dom manipulation in javascript</p>
+  function submitForm(e){
+   //form ko submit karne ke liye by default parameter hota hai jisko event khete hai , 
+   e.preventDefault();
+   console.log("input field 1 value controlled by ref", inputRef.current.value);
+   console.log("input field 2 value controlled by ref", inputRef2.current.value);
+   let input3 = document.getElementById("input3").value 
+
+   console.log("input field 3 value controlled by ref", input3);
+
+  }
+  return (
+    <div className="App">
+      <h1>UnControlled components in Reactjs</h1>
+      {/* <p>it is related to input field or in form there are 3-4 input fields </p>
+                  <p>react ke andar jab hum input fields ko directly state ke through control nhi krte hai to unko khete hai UnControlled component</p>
+                  <p>uncontrolled component wo hote hai jo ki directly dom ke through handle hote hai jaise ki jQuery or dom manipulation in javascript or Ref ke through control ho rhi hongi</p>
                  <p>value ko handle kese karnge?</p>
         
             */}
-            {/* ye input field state ke through Controlled ho rha hai */}
-           {/* hum value ko state se control kr rhe hai toh usme hum kya ek default value nhi lga skte ?hume default value ko apply krne ke liye val state ko hathana padega tabhi default value pass hogi but hum useState mai bhi default value initalize kr skte   hai = by default value set karne ke liye hum isme default value lga skte hai */}
-          <input type="text" value={val}  onChange={(e)=>setVal(e.target.value)}/>
-          {/* <input type="text" value={item} onChange={(e)=>setItem(e.target.value)}/> */}
 
-          <h3>Value : {val}</h3>
-          <h3>Item : {item}</h3>
-
-           </div>
-          
-         );
-
-
-
+      <form onSubmit={submitForm}>
+        <input ref={inputRef} type="text" />
+        <br />
+        <br />
+        <input  ref={inputRef2} type="text" /> <br />
+        <br />
+        <input id="input3" type="text" />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
