c0eefc284c9b125f4814aea558fa629348340cd9
sagar <rebelsagarx@gmail.com>
2022-02-28 15:48:10 +0530

    Controlled components in Reactjs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import "./App.css";
-import React,{useRef} from "react";
-import User from "./User"
+import React,{useState} from "react";
 
 function App  (){
-     
-  let inputRef = useRef(null)
+     let [val, setVal] = useState("000")
+     let [item, setItem] = useState("")
 
-  function updateInput(){
-inputRef.current.value ="1000"
-inputRef.current.style.color ="red"
-inputRef.current.focus()
-
-
-  }
       return (
         <div className="App">
-           <h1>forward Ref</h1>
-           {/* <p>if input is in child component and button is in parent component, and we want to change its dom , when button is clicked from parent component, so we use forward ref hook</p> */}
-            <User ref={inputRef}/>
-             <button onClick={updateInput}>update input box</button>
+           <h1>Controlled components in Reactjs</h1>
+                  {/* <p>it is related to input field or in form there are 3-4 input fields </p>
+                  <p>react ke andar jab hum input fields ko state ke through control krte hai to unko khete hai Controlled component</p>
+                  <p>uncontrolled component wo hote hai jo ki directly dom ke through handle hote hai jaise ki jQuery or dom manipulation in javascript</p>
+                 <p>value ko handle kese karnge?</p>
+        
+            */}
+            {/* ye input field state ke through Controlled ho rha hai */}
+           {/* hum value ko state se control kr rhe hai toh usme hum kya ek default value nhi lga skte ?hume default value ko apply krne ke liye val state ko hathana padega tabhi default value pass hogi but hum useState mai bhi default value initalize kr skte   hai = by default value set karne ke liye hum isme default value lga skte hai */}
+          <input type="text" value={val}  onChange={(e)=>setVal(e.target.value)}/>
+          {/* <input type="text" value={item} onChange={(e)=>setItem(e.target.value)}/> */}
+
+          <h3>Value : {val}</h3>
+          <h3>Item : {item}</h3>
 
            </div>
           
