9f09cb829e2b231276d14651b9ffe2b13d89cdd8
sagar <rebelsagarx@gmail.com>
2022-02-28 18:09:34 +0530

    use react router wrapper in index.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,29 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router,Link ,Route,Switch} from "react-router-dom";
+import Nav from "./Nav"
+import Home from "./Home"
+import About from "./About"
 
 function App() {
   return (
     <div className="App">
-      <Router>
-      <Link to="/home">Home Page</Link>
-      <Link to="/about">About Page</Link>
-    
-   <Switch>
-   <Route path="/home"> <Home /></Route>
-      <Route path="/about"> <About /></Route>
-   </Switch>
-
-  
+    {/* <h3>use react router wrapper in index.js</h3> */}
  
+     <Nav/>
+  
 
-   
+      <Route path="/" exact={true}> <Home /></Route>
+      <Route path="/about"> <About /></Route>
+
+
        
-      </Router>
+      
     </div>
   );
 }
 
-function Home() {
-  return (
-    <div>
-      {/* <h1>Routing setup </h1> */}
-      <h1>Home Page</h1>
-      <p>This is a Home Page</p>
-    </div>
-  );
-}
-function About() {
-  return (
-    <div>
-      <h1>About Page</h1>
-      <p>This is a About Page</p>
-    </div>
-  );
-}
+
+
 
 export default App;
